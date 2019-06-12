6de297139f17a2af0286c161d875fe11657779d8
Ivan Cantalice <ivanmonteiroc@gmail.com>
2019-06-11 21:34:28 -0300

    added scripts

---
+++ benchmark_googlenet.sh
@@ -0,0 +1,3 @@
+python3 pi_deep_learning.py --prototxt models/bvlc_googlenet.prototxt \
+	--model models/bvlc_googlenet.caffemodel --labels synset_words.txt \
+	--image images/barbershop.png
---
+++ benchmark_squeezenet.sh
@@ -0,0 +1,3 @@
+python3 pi_deep_learning.py --prototxt models/squeezenet_v1.0.prototxt \
+	--model models/squeezenet_v1.0.caffemodel --labels synset_words.txt \
+	--image images/barbershop.png
